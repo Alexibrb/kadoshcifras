5ef72a2427613f0722cad51cdfee7b96705fa35e
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2025-08-23 00:09:04 +0000

    quando instalo a app o favicon aparece na barra superior, mas quando ace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,13 @@ export default function RootLayout({
   return (
     <html lang="pt" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
