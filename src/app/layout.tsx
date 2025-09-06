5d8814a22b9d47d4b2bfbc52e13f17179afed596
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2025-09-06 21:39:07 +0000

    quando estou offline na verdade não consigo acessar nada no meu aplicati

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
     <html lang="pt" suppressHydrationWarning>
       <head>
         <meta name="theme-color" content="#9f50e5" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
