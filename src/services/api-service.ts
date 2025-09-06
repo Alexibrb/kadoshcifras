93f06114404b7f75412e6ab841ee6789e4e10590
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2025-09-06 15:43:05 +0000

    quando clica na sincronização só dois dados são salvos na songs

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -10,18 +10,32 @@ const MOCK_DATA = {
         songs: [
             { id: '1', title: 'Sonda-me, Usa-me', artist: 'Aline Barros', category: 'Adoração', genre: 'Gospel' },
             { id: '2', title: 'Ousado Amor', artist: 'Isaias Saad', category: 'Adoração', genre: 'Gospel' },
+            { id: '3', title: 'Lugar Secreto', artist: 'Gabriela Rocha', category: 'Adoração', genre: 'Gospel' },
+            { id: '4', title: 'Algo Novo', artist: 'Kemuel', category: 'Louvor', genre: 'Pop' },
+            { id: '5', title: 'A Casa É Sua', artist: 'Casa Worship', category: 'Adoração', genre: 'Gospel' },
+            { id: '6', title: 'Girassol', artist: 'Priscilla Alcantara', category: 'Pop', genre: 'Pop' },
+            { id: '7', title: 'Deus Proverá', artist: 'Gabriela Gomes', category: 'Adoração', genre: 'Gospel' },
+            { id: '8', title: 'Em Teus Braços', artist: 'Laura Souguellis', category: 'Adoração', genre: 'Gospel' }
         ],
         artists: [
             { id: '1', name: 'Aline Barros' },
             { id: '2', name: 'Isaias Saad' },
+            { id: '3', name: 'Gabriela Rocha' },
+            { id: '4', name: 'Kemuel' },
+            { id: '5', name: 'Casa Worship' },
+            { id: '6', name: 'Priscilla Alcantara' },
+            { id: '7', name: 'Gabriela Gomes' },
+            { id: '8', name: 'Laura Souguellis' }
         ],
         categories: [
             { id: '1', name: 'Adoração' },
             { id: '2', name: 'Louvor' },
+            { id: '3', name: 'Pop' }
         ],
         genres: [
             { id: '1', name: 'Gospel' },
             { id: '2', name: 'Pop Rock' },
+            { id: '3', name: 'Pop' }
         ]
     }
 };
